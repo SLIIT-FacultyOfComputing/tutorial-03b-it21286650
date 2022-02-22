8671a7e824f40e55ddc24dbfe3338081611767d7
it21286650 <99391707+it21286650@users.noreply.github.com>
2022-02-22 19:30:13 +0000

    I have Completed

--- exercise02.cpp
+++ exercise02.cpp
@@ -1,37 +1,32 @@
+
 #include <iostream>
 using namespace std;
 
 int volume(int height, int width, int length);
-
-struct box
+int main() 
 {
-	int Length;
-	int Width;
-	int Height;
-}box1,box2;
-
-int main() {
-    int box1Height, box1Width, box1Length;
+    int box1Height,box1Width,box1Length;
     int box2Height, box2Width, box2Length;
     int totalVolume, totalSurface;
     
+   
     cout << "Enter Box 1 Height : ";
-    cin >> box1.Height;
+    cin >> box1Height;
     cout << "Enter Box 1 Width : ";
-    cin >> box1.Width;
+    cin >> box1Width;
     cout << "Enter Box 1 Length : ";
-    cin >> box1.Length;
+    cin >> box1Length;
     
-     cout << "Enter Box 2 Height : ";
-    cin >> box2.Height;
+    cout << "Enter Box 2 Height : ";
+    cin >> box2Height;
     cout << "Enter Box 2 Width : ";
-    cin >> box2.Width;
+    cin >> box2Width;
     cout << "Enter Box 2 Length : ";
-    cin >> box2.Length;
+    cin >> box2Length;
     
-    totalVolume = volume(box1.Height, box1.Width, box1.Length) + volume(box2.Height, box2.Width, box2.Length);
-            
-             
+    totalVolume = volume(box1Height ,box1Width ,box1Length )
+             + volume(box2Height ,box2Width ,box2Length );
+    
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
